7e1450593ed674c6182cd75604d67346f6470fa0
Ian <github.angrily818@passmail.net>
2026-04-05 14:47:35 +0200

    feat: include referrer (#3)
    Co-authored-by: Ian <stylo.00@pm.me>

--- src/infotrafic/infotrafic.ts
+++ src/infotrafic/infotrafic.ts
@@ -4,6 +4,8 @@ import type { InfotraficResponse } from "./types.js";
 const BASE_URL = "https://plan.naolib.fr/api/poi/infotrafic";
 
 export async function getInfotrafic(): Promise<InfotraficResponse> {
-  const response = await axios.get<InfotraficResponse>(BASE_URL);
+  const response = await axios.get<InfotraficResponse>(BASE_URL, {
+    headers: { Referer: "https://plan.naolib.fr/" },
+  });
   return response.data;
 }
--- src/search/search.ts
+++ src/search/search.ts
@@ -6,6 +6,9 @@ const BASE_URL = "https://tanv4.cartographie.pro/api/autocomplete/itinerary";
 export async function search(
   params: SearchParams
 ): Promise<SearchResponse> {
-  const response = await axios.get<SearchResponse>(BASE_URL, { params });
+  const response = await axios.get<SearchResponse>(BASE_URL, {
+    params,
+    headers: { Referer: "https://plan.naolib.fr/" },
+  });
   return response.data;
 }
