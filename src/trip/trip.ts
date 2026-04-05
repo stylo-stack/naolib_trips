bef22efc36024eb998c517f5f699d7c84e788832
Ian <stylo.00@pm.me>
2026-04-05 14:46:13 +0200

    feat: include referrer

--- src/trip/trip.ts
+++ src/trip/trip.ts
@@ -4,6 +4,9 @@ import type { TripParams, TripResponse } from "./types.js";
 const BASE_URL = "https://plan.naolib.fr/api/itinerary/search";
 
 export async function searchTrip(params: TripParams): Promise<TripResponse> {
-  const response = await axios.get<TripResponse>(BASE_URL, { params });
+  const response = await axios.get<TripResponse>(BASE_URL, {
+    params,
+    headers: { Referer: "https://plan.naolib.fr/" },
+  });
   return response.data;
 }
